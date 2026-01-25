ed7763a110b00ec161daf0bab5fb415943f33cda
Moksh Barapatre <barapatremoksh1816@gmail.com>
2026-01-25 17:59:25 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (94.80%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -5,17 +5,15 @@ public:
 
         while (l <= r) {
             int mid = l + (r - l) / 2;
+
             if (nums[mid] == target) return mid;
 
-            // left half sorted
-            if (nums[l] <= nums[mid]) {
+            if (nums[l] <= nums[mid]) { // left sorted
                 if (nums[l] <= target && target < nums[mid])
                     r = mid - 1;
                 else
                     l = mid + 1;
-            }
-            // right half sorted
-            else {
+            } else { // right sorted
                 if (nums[mid] < target && target <= nums[r])
                     l = mid + 1;
                 else
