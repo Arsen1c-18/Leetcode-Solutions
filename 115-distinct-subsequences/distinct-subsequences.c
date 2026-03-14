239adb53b0b1ad12c2d53d0ced3176ea323dc314
Moksh Barapatre <barapatremoksh1816@gmail.com>
2026-03-15 01:09:03 +0530

    Time: 11 ms (73.71%) | Memory: 8.8 MB (58.86%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.c
@@ -0,0 +1,18 @@
+int numDistinct(char *s, char *t) {
+    int m = strlen(s), n = strlen(t);
+
+    unsigned long long dp[n+1];
+    for(int i=0;i<=n;i++)
+        dp[i] = 0;
+
+    dp[0] = 1;
+
+    for(int i=1;i<=m;i++){
+        for(int j=n;j>=1;j--){
+            if(s[i-1] == t[j-1])
+                dp[j] += dp[j-1];
+        }
+    }
+
+    return (int)dp[n];
+}
