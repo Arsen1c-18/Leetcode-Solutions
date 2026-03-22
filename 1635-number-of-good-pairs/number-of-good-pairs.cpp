fa88ac81fecae74c1d991444ca010be434d051c0
Moksh Barapatre <barapatremoksh1816@gmail.com>
2026-03-22 22:54:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (51.40%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -4,7 +4,7 @@ public:
         int count = 0;
         for(int i = 0; i< nums.size();i++){
             for(int j = 0; j<nums.size();j++){
-                if(nums[i] == nums[j] and i<j ){
+                if(nums[i] == nums[j] && i<j ){
                     count++;
                 }
             }
