366ab255762227a57d497072310087a693b587fb
Moksh Barapatre <barapatremoksh1816@gmail.com>
2026-03-30 00:24:38 +0530

    Time: 3 ms (10.46%) | Memory: 17 MB (28.81%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(nums.begin(),nums.end());
+        for (int i = 0 ; i<n ; i++){
+            ans.push_back(nums[i]); 
+        }
+        return ans;
+
+    }
+};
