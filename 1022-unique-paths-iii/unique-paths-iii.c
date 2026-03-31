3f028e416d66b7d878b61043ea2a6b7467b4baa5
Moksh Barapatre <barapatremoksh1816@gmail.com>
2026-03-31 20:21:19 +0530

    Time: 2 ms (10.00%) | Memory: 8.8 MB (28.18%) - LeetSync

--- 1022-unique-paths-iii/unique-paths-iii.c
+++ 1022-unique-paths-iii/unique-paths-iii.c
@@ -1,53 +1,44 @@
-int dfs(int** grid,int m,int n,int x,int y,int remain){
+#include <stdlib.h>
 
-    if(grid[x][y]==2){
-        if(remain==1) return 1;
+int dfs(int** grid, int r, int c, int m, int n, int remain) {
+
+    if(r < 0 || c < 0 || r >= m || c >= n || grid[r][c] == -1)
         return 0;
-    }
 
-    int temp = grid[x][y];
-    grid[x][y] = -1;
+    if(grid[r][c] == 2)
+        return remain == 1;
 
-    int paths = 0;
+    int temp = grid[r][c];
+    grid[r][c] = -1;
 
-    int d[4][2]={{1,0},{-1,0},{0,1},{0,-1}};
+    int paths =
+        dfs(grid,r+1,c,m,n,remain-1) +
+        dfs(grid,r-1,c,m,n,remain-1) +
+        dfs(grid,r,c+1,m,n,remain-1) +
+        dfs(grid,r,c-1,m,n,remain-1);
 
-    for(int i=0;i<4;i++){
-
-        int nx=x+d[i][0];
-        int ny=y+d[i][1];
-
-        if(nx>=0 && ny>=0 && nx<m && ny<n && grid[nx][ny]!=-1){
-
-            paths += dfs(grid,m,n,nx,ny,remain-1);
-        }
-    }
-
-    grid[x][y]=temp;
+    grid[r][c] = temp;
 
     return paths;
 }
 
-int uniquePathsIII(int** grid,int gridSize,int* gridColSize){
+int uniquePathsIII(int** grid, int gridSize, int* gridColSize) {
 
     int m = gridSize;
     int n = gridColSize[0];
 
-    int sx=0,sy=0;
-    int cells=0;
+    int startR, startC;
+    int count = 0;
 
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
-
-            if(grid[i][j]!=-1)
-                cells++;
-
-            if(grid[i][j]==1){
-                sx=i;
-                sy=j;
+            if(grid[i][j] != -1) count++;
+            if(grid[i][j] == 1){
+                startR = i;
+                startC = j;
             }
         }
     }
 
-    return dfs(grid,m,n,sx,sy,cells);
+    return dfs(grid,startR,startC,m,n,count);
 }
