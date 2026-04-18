a317f46daac2b69045b282960d7ba929797e50c0
Moksh Barapatre <barapatremoksh1816@gmail.com>
2026-04-18 16:18:10 +0530

    Time: 4 ms (32.08%) | Memory: 8.5 MB (64.48%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int maxLength = max(word1.length(),word2.length());
+        string merged;
+        for (int i = 0 ; i < maxLength; i++){
+            if(i<word1.length()){
+                merged.push_back(word1[i]);
+            }
+            if(i<word2.length()){
+                merged.push_back(word2[i]);
+            }
+        }
+        return merged;
+    }
+};
